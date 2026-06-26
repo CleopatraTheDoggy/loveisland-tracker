178657a44bbb4efb2aa9e8f095a7f984c75c1e01
CleopatraTheDoggy <cleopatrathedoggy@gmail.com>
2026-06-25 21:04:41 -0500

    V3.4.8

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const DUMPED_CONTESTANTS = {
     "parm3_sean": "2026-06-08T03:05:25.446900Z",
     "bhatz_track": "2026-06-13T03:37:19.090869Z",
     "itssoll": "2026-06-19T03:08:29.699441Z",
-    "gvasconcelosv": "2026-06-19T03:08:29.699441Z"
-    // "alannahkeyser": "" 
+    "gvasconcelosv": "2026-06-19T03:08:29.699441Z",
+    "alannahkeyser": "2026-06-26T02:00:11.745000Z" 
 };
 
 const BOMBSHELL_CONTESTANTS = {
